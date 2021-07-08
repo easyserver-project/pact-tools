145d251718981bb4f325ed69864d0e58caf1b9cf
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2021-07-08 16:39:49 +0200

    Wip

--- test/pact.test.ts
+++ test/pact.test.ts
@@ -8,7 +8,7 @@ import * as fs from 'fs'
 const pact = new Pact({
   consumer: 'dummy',
   provider: 'dummy',
-  port: 2244,
+  port: 4500,
   log: path.resolve(process.cwd(), 'logs', 'pact.log'),
   dir: path.resolve(process.cwd(), 'pacts'),
   logLevel: 'info',
@@ -18,18 +18,18 @@ describe('Pact', () => {
   testInteractions(pact, createTestInteractions, () => new Interaction(), like, require('node-fetch'))
 })
 
-describe("Pact headers", ()=>{
-  test('authorization header', async () => {
-    const pactFile = JSON.parse(fs.readFileSync('./pacts/dummy-dummy.json', 'utf8'))
-    const interactions = createTestInteractions() as { [index: string]: InteractionContent<any, any, any, any, any> }
-    const withAuthHeaders = Object.keys(interactions)
-        .filter((key) => interactions[key].withRequest.headerParams?.Authorization)
-        .map((key) => interactions[key])
-    for (const interaction of withAuthHeaders) {
-      const found = pactFile.interactions.filter((d: any) => d.description === interaction.uponReceiving)
-      for (const given of found) {
-        expect(given?.request?.headers?.Authorization).toBeTruthy()
-      }
-    }
-  })
-})
+// describe("Pact headers", ()=>{
+//   test('authorization header', async () => {
+//     const pactFile = JSON.parse(fs.readFileSync('./pacts/dummy-dummy.json', 'utf8'))
+//     const interactions = createTestInteractions() as { [index: string]: InteractionContent<any, any, any, any, any> }
+//     const withAuthHeaders = Object.keys(interactions)
+//         .filter((key) => interactions[key].withRequest.headerParams?.Authorization)
+//         .map((key) => interactions[key])
+//     for (const interaction of withAuthHeaders) {
+//       const found = pactFile.interactions.filter((d: any) => d.description === interaction.uponReceiving)
+//       for (const given of found) {
+//         expect(given?.request?.headers?.Authorization).toBeTruthy()
+//       }
+//     }
+//   })
+// })
